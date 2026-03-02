8729c3a7d3e99cd2241f78f52ae8ebec3baf2275
Leo <leonardo2002gonzalez@gmail.com>
2026-03-02 20:31:08 -0300

    refactor: update API endpoints to use environment variable for flexibility

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login({ setUser }) {
     const token = credentialResponse.credential;
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/google-login/", {
+      const res = await fetch("${import.meta.env.VITE_API_URL}/api/google-login/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ token }),
@@ -53,7 +53,8 @@ function Login({ setUser }) {
     setError("");
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/token/", {
+      console.log("API URL:", import.meta.env.VITE_API_URL)
+      const res = await fetch("${import.meta.env.VITE_API_URL}/api/token/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
